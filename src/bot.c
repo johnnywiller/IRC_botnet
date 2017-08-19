c16d4aa7959dad27d751bcc68cfeb187ceb4dfd2
Johnny Willer <johnnywiller10@gmail.com>
2017-08-19 19:05:43 -0300

    implementing command execution through IRC command

--- src/bot.c
+++ src/bot.c
@@ -22,7 +22,7 @@ struct irc_desc {
 	char *port;
 };
 
-static int irc_connect();
+static int irc_connect(struct irc_desc *desc);
 static int irc_send(char *msg, int fd);
 static void irc_login(struct irc_desc *desc);
 static void *irc_listen(void *arg);
@@ -41,7 +41,6 @@ int main(int argc, char* argv[]) {
 	pthread_t t_irc, t_stdin;
 
 	struct irc_desc *desc = malloc(sizeof(struct irc_desc));
-
 
 	while((opt = getopt(argc, argv, "h:p:n:c:")) != -1) {
 		if (opt == '?')	{
@@ -69,20 +68,22 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
-	if ((fd_irc = irc_connect()) == -1) {
-		perror("cannot connect to IRC");
-		exit(EXIT_FAILURE);
-	}
-
 	desc->ch_name = ch_name;
 	desc->nick = nick;
 	desc->hostname = hostname;
 	desc->port = port;
+
+	if ((fd_irc = irc_connect(desc)) == -1) {
+		perror("cannot connect to IRC");
+		exit(EXIT_FAILURE);
+	}
+
 	desc->fd = fd_irc;
 
 	irc_login(desc);
+	puts("logged into IRC");
 
-	pthread_create(&t_irc, NULL, irc_listen, &fd_irc);
+	pthread_create(&t_irc, NULL, irc_listen, desc);
 	puts("thread irc_listen created");
 
 	pthread_create(&t_stdin, NULL, stdin_listen, desc);
@@ -95,7 +96,7 @@ int main(int argc, char* argv[]) {
 
 static void *stdin_listen(void *arg) {
 
-	struct irc_desc *desc = (struct irc_desc*)arg;
+	struct irc_desc *desc = (struct irc_desc *)arg;
 	int fd_irc = desc->fd;
 	int num_read;
 	char buf[MAX_BUF];
@@ -113,13 +114,37 @@ static void *stdin_listen(void *arg) {
 
 static void *irc_listen(void *arg) {
 
-	int fd_irc  = *((int*) arg);
+	struct irc_desc *desc = (struct irc_desc *) arg;
 	int num_read;
-	char buf[MAX_BUF], message[MAX_BUF + 1];
+	int aux_space = strlen(desc->ch_name) + strlen("Executing command: ") + 3;
+	char buf[MAX_BUF], message[MAX_BUF + aux_space], popen_buf[MAX_BUF + 1];
+	char *str_ret;
+	FILE *file_popen;
 
-	while((num_read = read(fd_irc, buf, sizeof(buf))) > 0) {
-	       snprintf(message, num_read, "%s", buf);
-	       puts(message);
+	while((num_read = read(desc->fd, buf, sizeof(buf))) > 0) {
+		// set the terminating null byte of string
+		buf[num_read] = 0;
+
+		if ((str_ret = strstr(buf, "PRIVMSG")) != NULL) {
+			if ((str_ret = strstr(str_ret, ":!")) != NULL) {
+				snprintf(message, num_read + aux_space, "%s :Executing command: %s", desc->ch_name, &(str_ret[2]));
+				irc_send_priv(message, desc->fd);
+				snprintf(message, strlen(str_ret)-3, "%s", (str_ret+2));
+				if ((file_popen = popen(message, "r")) == NULL) {
+					perror("popen");
+					snprintf(message, num_read + aux_space + 6, "%s :ERROR executing command: %s", desc->ch_name, &(str_ret[2]));
+					irc_send_priv(message, desc->fd);
+					continue;
+
+				} else {
+
+					while(fgets(popen_buf, sizeof(popen_buf), file_popen) != NULL)  {
+						snprintf(message, strlen(popen_buf) + strlen(desc->ch_name) + 3, "%s :%s", desc->ch_name, popen_buf);
+						irc_send_priv(message, desc->fd);
+					}
+				}
+			}
+		}
 	}
 }
 
@@ -186,42 +211,49 @@ static int irc_send(char *msg, int fd) {
 	return 0;
 }
 
-static int irc_connect() {
+static int irc_connect(struct irc_desc *desc) {
 
 	int fd_irc;
-      	struct addrinfo hints, *result, *rp;
+      	// structures used to get host info, like IP e port
+	struct addrinfo hints, *result, *rp;
 
+	// hints is used to filter getaddrinfo response
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_canonname = NULL;
 	hints.ai_addr = NULL;
 	hints.ai_next = NULL;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM; // we want TCP
+	hints.ai_family = AF_INET; // only IPv4, put AF_INET6 to IPv6 or AF_UNSPEC to get both
 	hints.ai_flags = 0;
 
-	if (getaddrinfo(IRC_HOST, IRC_PORT, &hints, &result) != 0) {
+	// try to find a socket
+	if (getaddrinfo(desc->hostname, desc->port, &hints, &result) != 0) {
 		perror("getaddrinfo");
+		puts("falhou getaddr");
 		exit(EXIT_FAILURE);
 	}
 
-
+	// search through responses
 	for (rp = result; rp != NULL; rp = rp->ai_next) {
-		printf("%d\n", rp->ai_family);
 		if ((fd_irc = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol)) == -1) {
 			perror("irc socket");
-			//exit(EXIT_FAILURE);
+			// if we can't create a socket with some response, no problem try next
 			continue;
 		}
 
+		// we've successfuly create a socket, now we need to try to connect
 		if (connect(fd_irc, rp->ai_addr, rp->ai_addrlen) == -1) {
 			perror("irc connect");
-			continue;
+			continue; // again no problem if not works
 		}
 		break;
 	}
 
+	// we must use this convenient method to free all results list
+	// (since is a linked list cannot be made with a simple free() )
 	freeaddrinfo(result);
 
+	// arrived at last result without successful connection
 	if (rp == NULL) {
 		close(fd_irc);
 		perror("cannot connect to irc");
